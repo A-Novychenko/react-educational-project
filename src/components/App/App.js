3ac6eda2a56574777a799b65a6ad08ddecb62a07
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-03-12 01:04:50 +0200

    routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,32 @@
 import { Home } from 'pages/Home';
+import { NotFound } from 'pages/NotFound';
 import { Projects } from 'pages/Projects';
 import { Stack } from 'pages/Stack';
-import { GlobalStyle } from '../../constants/GlobalStyle';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
 import { Container } from './App.styled';
 
 export const App = () => {
   return (
     <Container>
-      <Home />
-      <Stack />
-      <GlobalStyle />
-      <Projects />
+      <div>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/stack">Stack</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/stack" element={<Stack />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+
+      {/* <Home />
+        <Stack />
+        <Projects /> */}
     </Container>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App';
+import { GlobalStyle } from './constants/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <GlobalStyle />
     </BrowserRouter>
   </React.StrictMode>
 );
